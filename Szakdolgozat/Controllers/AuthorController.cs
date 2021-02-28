a5f66940abab8d38acfaa94fa785c0ff6b06b619
Jorghy <36571093+Jorghy@users.noreply.github.com>
2021-02-28 21:14:51 +0100

    Szerző CRUD metódusok

--- Szakdolgozat/Controllers/AuthorController.cs
+++ Szakdolgozat/Controllers/AuthorController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using Szakdolgozat.BL.UnitOfWork;
+using Szakdolgozat.DTO.Models;
 using Szakdolgozat.DTO.ViewModels;
 
 namespace Szakdolgozat.Web.Controllers
@@ -21,7 +23,106 @@ namespace Szakdolgozat.Web.Controllers
         {
             var authors = _unitOfWork.Authors.GetAll();
             var authorsVm = _mapper.Map<List<AuthorVm>>(authors);
+            ViewData["Message"] = TempData["Message"];
             return View(authorsVm);
         }
+
+        #region Add
+        public IActionResult Create(int? id)
+        {
+            if (id == null)
+                return View();
+
+            var azon = int.Parse(id.ToString());
+            var author = _unitOfWork.Authors.GetById(azon);
+            if (author == null)
+                return NotFound();
+
+            var authorVm = _mapper.Map<AuthorVm>(author);
+            return View(authorVm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(AuthorVm form)
+        {
+            if (ModelState.IsValid)
+            {
+                AuthorCo formCo = _mapper.Map<AuthorCo>(form);
+                _unitOfWork.Authors.Add(formCo);
+                _unitOfWork.Complate();
+                TempData["Message"] = "Sikeres hozzáadás!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(form);
+        }
+        #endregion
+
+        #region Edit
+        public IActionResult Edit(int id)
+        {
+            return RedirectToAction(nameof(Create), new { id = id });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(AuthorVm form)
+        {
+            if (ModelState.IsValid)
+            {
+                AuthorCo authorCo = _mapper.Map<AuthorCo>(form);
+                try
+                {
+                    _unitOfWork.Authors.Update(authorCo);
+                    _unitOfWork.Complate();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (_unitOfWork.Authors.GetById(authorCo.AuthorId) == null)
+                        return NotFound();
+                }
+                TempData["Message"] = "A változtatásokat sikeresen elmentettük!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(form);
+        }
+        #endregion
+
+        #region Details
+        public IActionResult Details(int id)
+        {
+            var authorCo = _unitOfWork.Authors.GetById(id);
+
+            if (authorCo == null)
+                return NotFound();
+
+            var authorVm = _mapper.Map<AuthorVm>(authorCo);
+            return View(authorVm);
+        }
+        #endregion
+
+        #region Delete
+        public IActionResult Delete(int id)
+        {
+            var authorCo = _unitOfWork.Authors.GetById(id);
+
+            if (authorCo == null)
+                return NotFound();
+
+            var authorVm = _mapper.Map<AuthorVm>(authorCo);
+            return View(authorVm);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var authorCo = _unitOfWork.Authors.GetById(id);
+            _unitOfWork.Authors.Delete(authorCo);
+            _unitOfWork.Complate();
+            TempData["Message"] = "Sikeres törlés!";
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
     }
 }
